0c3a36cebed83a6e9535bf22063c4aee4c9543ce
mariasanmartinceli <mariasanmartinceli@gmail.com>
2025-05-01 16:48:46 -0400

    Update biomimimetic_design.js

--- biomimimetic_design.js
+++ biomimimetic_design.js
@@ -155,7 +155,7 @@
       function setup() {
         createCanvas(1500,800);
         background("rgb(0, 28, 0)");
-        var intervalID = window.setInterval(newEllipse,5000);
+        var intervalID = window.setInterval(newEllipse,4000);
         fade=0;
       }
       function draw() {
